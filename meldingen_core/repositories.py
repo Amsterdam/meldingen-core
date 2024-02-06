6097037806089e29709ef04d71194751993900db
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-06 10:56:12 +0100

    Introduce generic base repository (#5)

--- meldingen_core/repositories.py
+++ meldingen_core/repositories.py
@@ -1,19 +1,25 @@
 from abc import ABCMeta, abstractmethod
+from typing import Generic, TypeVar
 
 from meldingen_core.models import Melding
 
+T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
 
-class BaseMeldingRepository(metaclass=ABCMeta):
+
+class BaseRepository(Generic[T, T_co], metaclass=ABCMeta):
+    @abstractmethod
+    def add(self, obj: T) -> None:
+        ...
+
+    @abstractmethod
+    def list(self, *, limit: int | None = None, offset: int | None = None) -> list[T_co]:
+        ...
+
+    @abstractmethod
+    def retrieve(self, pk: int) -> T_co | None:
+        ...
+
+
+class BaseMeldingRepository(BaseRepository[Melding, Melding], metaclass=ABCMeta):
     """Repository for Melding."""
-
-    @abstractmethod
-    def add(self, melding: Melding) -> None:
-        ...
-
-    @abstractmethod
-    def list(self, *, limit: int | None = None, offset: int | None = None) -> list[Melding]:
-        ...
-
-    @abstractmethod
-    def retrieve(self, pk: int) -> Melding | None:
-        ...
