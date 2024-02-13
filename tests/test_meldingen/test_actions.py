edd05f1ba7cd28e9fb6286612a6206afcebb32f3
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-13 14:54:54 +0100

    Convert tests to async

--- tests/test_meldingen/test_actions.py
+++ tests/test_meldingen/test_actions.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_asyncio
 
 from meldingen_core.actions import MeldingCreateAction, MeldingListAction, MeldingRetrieveAction
 from meldingen_core.models import Melding
@@ -15,10 +16,10 @@ def unpopulated_meldingen_repository() -> BaseMeldingRepository:
         def __init__(self) -> None:
             self.data = []
 
-        def add(self, melding: Melding) -> None:
+        async def add(self, melding: Melding) -> None:
             self.data.append(melding)
 
-        def list(self, *, limit: int | None = None, offset: int | None = None) -> list[Melding]:
+        async def list(self, *, limit: int | None = None, offset: int | None = None) -> list[Melding]:
             if limit and offset:
                 return self.data[offset : offset + limit]
             elif limit and not offset:
@@ -28,7 +29,7 @@ def unpopulated_meldingen_repository() -> BaseMeldingRepository:
             else:
                 return self.data
 
-        def retrieve(self, pk: int) -> Melding | None:
+        async def retrieve(self, pk: int) -> Melding | None:
             for _melding in self.data:
                 if _melding.text == str(pk):
                     return _melding
@@ -38,14 +39,14 @@ def unpopulated_meldingen_repository() -> BaseMeldingRepository:
     return repository
 
 
-@pytest.fixture
-def populated_meldingen_repository(
+@pytest_asyncio.fixture
+async def populated_meldingen_repository(
     unpopulated_meldingen_repository: BaseMeldingRepository,
 ) -> BaseMeldingRepository:
     for _pk in range(10):
         melding = Melding()
         melding.text = f"{_pk}"
-        unpopulated_meldingen_repository.add(melding)
+        await unpopulated_meldingen_repository.add(melding)
 
     return unpopulated_meldingen_repository
 
@@ -75,21 +76,23 @@ def meldingen_retrieve_action(
 
 
 class TestMeldingCreateAction:
-    def test_add(self, meldingen_create_action: MeldingCreateAction) -> None:
-        assert len(meldingen_create_action.repository.list()) == 0
+    @pytest.mark.asyncio
+    async def test_add(self, meldingen_create_action: MeldingCreateAction) -> None:
+        assert len(await meldingen_create_action.repository.list()) == 0
 
         melding = Melding()
         melding.text = "1"
 
-        meldingen_create_action(melding)
+        await meldingen_create_action(melding)
 
-        assert len(meldingen_create_action.repository.list()) == 1
-        assert meldingen_create_action.repository.retrieve(pk=1) == melding
+        assert len(await meldingen_create_action.repository.list()) == 1
+        assert await meldingen_create_action.repository.retrieve(pk=1) == melding
 
 
 class TestMeldingListAction:
-    def test_list_all(self, meldingen_list_action: MeldingListAction) -> None:
-        meldingen = meldingen_list_action()
+    @pytest.mark.asyncio
+    async def test_list_all(self, meldingen_list_action: MeldingListAction) -> None:
+        meldingen = await meldingen_list_action()
 
         assert len(meldingen) == 10
 
@@ -97,19 +100,21 @@ class TestMeldingListAction:
         "limit, expected_result",
         [(1, 1), (5, 5), (10, 10), (20, 10)],
     )
-    def test_list_limit(self, meldingen_list_action: MeldingListAction, limit: int, expected_result: int) -> None:
-        meldingen = meldingen_list_action(limit=limit)
+    @pytest.mark.asyncio
+    async def test_list_limit(self, meldingen_list_action: MeldingListAction, limit: int, expected_result: int) -> None:
+        meldingen = await meldingen_list_action(limit=limit)
 
         assert len(meldingen) == expected_result
 
     @pytest.mark.parametrize("offset, expected_result", [(1, 9), (5, 5), (10, 0), (20, 0)])
-    def test_list_offset(
+    @pytest.mark.asyncio
+    async def test_list_offset(
         self,
         meldingen_list_action: MeldingListAction,
         offset: int,
         expected_result: int,
     ) -> None:
-        meldingen = meldingen_list_action(offset=offset)
+        meldingen = await meldingen_list_action(offset=offset)
 
         assert len(meldingen) == expected_result
 
@@ -117,28 +122,33 @@ class TestMeldingListAction:
         "limit, offset, expected_result",
         [(10, 0, 10), (5, 0, 5), (10, 10, 0), (20, 0, 10)],
     )
-    def test_list_limit_offset(
+    @pytest.mark.asyncio
+    async def test_list_limit_offset(
         self,
         meldingen_list_action: MeldingListAction,
         limit: int,
         offset: int,
         expected_result: int,
     ) -> None:
-        meldingen = meldingen_list_action(limit=limit, offset=offset)
+        meldingen = await meldingen_list_action(limit=limit, offset=offset)
 
         assert len(meldingen) == expected_result
 
 
 class TestMeldingRetrieveAction:
     @pytest.mark.parametrize("pk", [1, 2, 3, 4, 5])
-    def test_retrieve_existing_meldingen(self, meldingen_retrieve_action: MeldingRetrieveAction, pk: int) -> None:
-        melding = meldingen_retrieve_action(pk=pk)
+    @pytest.mark.asyncio
+    async def test_retrieve_existing_meldingen(self, meldingen_retrieve_action: MeldingRetrieveAction, pk: int) -> None:
+        melding = await meldingen_retrieve_action(pk=pk)
 
         assert melding is not None
         assert melding.text == str(pk)
 
     @pytest.mark.parametrize("pk", [101, 102, 103, 104, 105])
-    def test_retrieve_non_existing_meldingen(self, meldingen_retrieve_action: MeldingRetrieveAction, pk: int) -> None:
-        melding = meldingen_retrieve_action(pk=pk)
+    @pytest.mark.asyncio
+    async def test_retrieve_non_existing_meldingen(
+        self, meldingen_retrieve_action: MeldingRetrieveAction, pk: int
+    ) -> None:
+        melding = await meldingen_retrieve_action(pk=pk)
 
         assert melding is None
