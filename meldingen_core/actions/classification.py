830850a8a70ce1c6c9fbed8ecc5cda355f049162
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-28 15:15:35 +0100

    Allow for derived classification models in list and retrieve actions (#15)

--- meldingen_core/actions/classification.py
+++ meldingen_core/actions/classification.py
@@ -1,3 +1,5 @@
+from typing import TypeVar
+
 from meldingen_core.actions.base import (
     BaseCreateAction,
     BaseDeleteAction,
@@ -7,16 +9,19 @@ from meldingen_core.actions.base import (
 )
 from meldingen_core.models import Classification
 
+T = TypeVar("T", bound=Classification)
+T_co = TypeVar("T_co", covariant=True, bound=Classification)
+
 
 class ClassificationCreateAction(BaseCreateAction[Classification, Classification]):
     ...
 
 
-class ClassificationListAction(BaseListAction[Classification, Classification]):
+class ClassificationListAction(BaseListAction[T, T_co]):
     ...
 
 
-class ClassificationRetrieveAction(BaseRetrieveAction[Classification, Classification]):
+class ClassificationRetrieveAction(BaseRetrieveAction[T, T_co]):
     ...
 
 
