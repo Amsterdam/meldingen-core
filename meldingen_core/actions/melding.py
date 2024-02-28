f915d7b923bf4ae3ef4816c46d2ec0f60b75d41b
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-28 09:16:19 +0100

    Mark models as dataclasses (#13)
    * Mark models as dataclasses
    
    * Allow specifying derived user types for action
    
    * Allow specifying derived user types for action
    
    * Allow specifying derived melding types for action
    
    * Cleanup tests

--- meldingen_core/actions/melding.py
+++ meldingen_core/actions/melding.py
@@ -1,14 +1,19 @@
+from typing import TypeVar
+
 from meldingen_core.actions.base import BaseCreateAction, BaseListAction, BaseRetrieveAction
 from meldingen_core.models import Melding
+
+T = TypeVar("T", bound=Melding)
+T_co = TypeVar("T_co", covariant=True, bound=Melding)
 
 
 class MeldingCreateAction(BaseCreateAction[Melding, Melding]):
     """Action that stores a melding."""
 
 
-class MeldingListAction(BaseListAction[Melding, Melding]):
+class MeldingListAction(BaseListAction[T, T_co]):
     """Action that retrieves a list of meldingen."""
 
 
-class MeldingRetrieveAction(BaseRetrieveAction[Melding, Melding]):
+class MeldingRetrieveAction(BaseRetrieveAction[T, T_co]):
     """Action that retrieves a melding."""
