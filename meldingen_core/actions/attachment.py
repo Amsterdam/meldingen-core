35ce7bd3334c3eebd313879e49cb59e4fe1f0297
Youri Westerman <y.westerman@amsterdam.nl>
2024-07-08 13:52:15 +0200

    Add base directory

--- meldingen_core/actions/attachment.py
+++ meldingen_core/actions/attachment.py
@@ -14,16 +14,19 @@ class UploadAttachmentAction(Generic[M, M_co]):
     _attachment_repository: BaseAttachmentRepository
     _melding_repository: BaseMeldingRepository[M, M_co]
     _filesystem: Filesystem
+    _base_directory: str
 
     def __init__(
         self,
         attachment_repository: BaseAttachmentRepository,
         melding_repository: BaseMeldingRepository[M, M_co],
         filesystem: Filesystem,
+        base_directory: str,
     ):
         self._attachment_repository = attachment_repository
         self._melding_repository = melding_repository
         self._filesystem = filesystem
+        self._base_directory = base_directory
 
     async def __call__(self, melding_id: int, original_filename: str, data: bytes) -> Attachment:
         melding = await self._melding_repository.retrieve(melding_id)
@@ -31,7 +34,9 @@ class UploadAttachmentAction(Generic[M, M_co]):
             raise NotFoundException("Melding not found")
 
         attachment = Attachment(original_filename, melding)
-        attachment.file_path = attachment.unique_identifier.replace("-", "/") + "/" + original_filename
+        attachment.file_path = (
+            f"/{self._base_directory}/{attachment.unique_identifier.replace("-", "/")}/{original_filename}"
+        )
 
         await self._filesystem.write(attachment.file_path, data)
 
