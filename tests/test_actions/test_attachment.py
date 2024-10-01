370c8b8fc17b81d2d2b31d1e3ae34bab4045d4ab
Youri Westerman <y.westerman@amsterdam.nl>
2024-10-01 16:20:37 +0200

    Group tests for upload action together in their own class

--- tests/test_actions/test_attachment.py
+++ tests/test_actions/test_attachment.py
@@ -11,50 +11,51 @@ from meldingen_core.repositories import BaseAttachmentRepository, BaseMeldingRep
 from meldingen_core.token import TokenVerifier
 
 
-@pytest.mark.anyio
-async def test_melding_not_found() -> None:
-    melding_repository = Mock(BaseMeldingRepository)
-    melding_repository.retrieve.return_value = None
+class TestUploadAttachmentAction:
+    @pytest.mark.anyio
+    async def test_melding_not_found(self) -> None:
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = None
 
-    action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
-        Mock(BaseAttachmentFactory),
-        Mock(BaseAttachmentRepository),
-        melding_repository,
-        Mock(Filesystem),
-        Mock(TokenVerifier),
-        "/tmp",
-    )
+        action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
+            Mock(BaseAttachmentFactory),
+            Mock(BaseAttachmentRepository),
+            melding_repository,
+            Mock(Filesystem),
+            Mock(TokenVerifier),
+            "/tmp",
+        )
 
-    with pytest.raises(NotFoundException) as exception_info:
-        await action(123, "super_secret_token", "original_filename.ext", b"Hello World!")
+        with pytest.raises(NotFoundException) as exception_info:
+            await action(123, "super_secret_token", "original_filename.ext", b"Hello World!")
 
-    assert str(exception_info.value) == "Melding not found"
+        assert str(exception_info.value) == "Melding not found"
 
 
-@pytest.mark.anyio
-async def test_can_handle_attachment() -> None:
-    melding = Melding("melding text")
+    @pytest.mark.anyio
+    async def test_can_handle_attachment(self) -> None:
+        melding = Melding("melding text")
 
-    melding_repository = Mock(BaseMeldingRepository)
-    melding_repository.retrieve.return_value = melding
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = melding
 
-    filesystem = Mock(Filesystem)
+        filesystem = Mock(Filesystem)
 
-    attachment_repository = Mock(BaseAttachmentRepository)
+        attachment_repository = Mock(BaseAttachmentRepository)
 
-    action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
-        Mock(BaseAttachmentFactory),
-        attachment_repository,
-        melding_repository,
-        filesystem,
-        Mock(TokenVerifier),
-        "/tmp",
-    )
+        action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
+            Mock(BaseAttachmentFactory),
+            attachment_repository,
+            melding_repository,
+            filesystem,
+            Mock(TokenVerifier),
+            "/tmp",
+        )
 
-    data = b"Hello World!"
+        data = b"Hello World!"
 
-    attachment = await action(123, "super_secret_token", "original_filename.ext", data)
+        attachment = await action(123, "super_secret_token", "original_filename.ext", data)
 
-    filesystem.makedirs.assert_awaited_once()
-    filesystem.write.assert_awaited_once_with(attachment.file_path, data)
-    attachment_repository.save.assert_awaited_once_with(attachment)
+        filesystem.makedirs.assert_awaited_once()
+        filesystem.write.assert_awaited_once_with(attachment.file_path, data)
+        attachment_repository.save.assert_awaited_once_with(attachment)
