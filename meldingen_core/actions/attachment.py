976fed8f88e4acd6cf8bf6ba432e802738c6ea08
Youri Westerman <y.westerman@amsterdam.nl>
2024-10-04 10:05:50 +0200

    Download attachment action (#72)
    * Add attachment download action
    
    * Group tests for upload action together in their own class
    
    * Add tests for download attachment action

--- meldingen_core/actions/attachment.py
+++ meldingen_core/actions/attachment.py
@@ -1,6 +1,7 @@
-from typing import Generic, TypeVar
+from typing import AsyncIterator, Generic, TypeVar
 from uuid import uuid4
 
+from plugfs import filesystem
 from plugfs.filesystem import Filesystem
 
 from meldingen_core.exceptions import NotFoundException
@@ -55,3 +56,42 @@ class UploadAttachmentAction(Generic[A, M, M_co]):
         await self._attachment_repository.save(attachment)
 
         return attachment
+
+
+class DownloadAttachmentAction(Generic[M, M_co]):
+    _melding_repository: BaseMeldingRepository[M, M_co]
+    _verify_token: TokenVerifier[M]
+    _attachment_repository: BaseAttachmentRepository
+    _filesystem: Filesystem
+
+    def __init__(
+        self,
+        melding_repository: BaseMeldingRepository[M, M_co],
+        token_verifier: TokenVerifier[M],
+        attachment_repository: BaseAttachmentRepository,
+        filesystem: Filesystem,
+    ):
+        self._melding_repository = melding_repository
+        self._verify_token = token_verifier
+        self._attachment_repository = attachment_repository
+        self._filesystem = filesystem
+
+    async def __call__(self, melding_id: int, attachment_id: int, token: str) -> AsyncIterator[bytes]:
+        melding = await self._melding_repository.retrieve(melding_id)
+        if melding is None:
+            raise NotFoundException("Melding not found")
+
+        self._verify_token(melding, token)
+
+        attachment = await self._attachment_repository.retrieve(attachment_id)
+        if attachment is None:
+            raise NotFoundException("Attachment not found")
+
+        if attachment.melding != melding:
+            raise NotFoundException(f"Melding with id {melding_id} does not have attachment with id {attachment_id}")
+
+        try:
+            file = await self._filesystem.get_file(attachment.file_path)
+            return await file.get_iterator()
+        except filesystem.NotFoundException as exception:
+            raise NotFoundException("File not found") from exception
--- tests/test_actions/test_attachment.py
+++ tests/test_actions/test_attachment.py
@@ -1,9 +1,10 @@
 from unittest.mock import Mock
 
 import pytest
-from plugfs.filesystem import Filesystem
+from plugfs import filesystem
+from plugfs.filesystem import File, Filesystem
 
-from meldingen_core.actions.attachment import UploadAttachmentAction
+from meldingen_core.actions.attachment import DownloadAttachmentAction, UploadAttachmentAction
 from meldingen_core.exceptions import NotFoundException
 from meldingen_core.factories import BaseAttachmentFactory
 from meldingen_core.models import Attachment, Melding
@@ -11,50 +12,163 @@ from meldingen_core.repositories import BaseAttachmentRepository, BaseMeldingRep
 from meldingen_core.token import TokenVerifier
 
 
-@pytest.mark.anyio
-async def test_melding_not_found() -> None:
-    melding_repository = Mock(BaseMeldingRepository)
-    melding_repository.retrieve.return_value = None
+class TestUploadAttachmentAction:
+    @pytest.mark.anyio
+    async def test_melding_not_found(self) -> None:
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = None
 
-    action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
-        Mock(BaseAttachmentFactory),
-        Mock(BaseAttachmentRepository),
-        melding_repository,
-        Mock(Filesystem),
-        Mock(TokenVerifier),
-        "/tmp",
-    )
+        action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
+            Mock(BaseAttachmentFactory),
+            Mock(BaseAttachmentRepository),
+            melding_repository,
+            Mock(Filesystem),
+            Mock(TokenVerifier),
+            "/tmp",
+        )
 
-    with pytest.raises(NotFoundException) as exception_info:
-        await action(123, "super_secret_token", "original_filename.ext", b"Hello World!")
+        with pytest.raises(NotFoundException) as exception_info:
+            await action(123, "super_secret_token", "original_filename.ext", b"Hello World!")
 
-    assert str(exception_info.value) == "Melding not found"
+        assert str(exception_info.value) == "Melding not found"
+
+    @pytest.mark.anyio
+    async def test_can_handle_attachment(self) -> None:
+        melding = Melding("melding text")
+
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = melding
+
+        filesystem = Mock(Filesystem)
+
+        attachment_repository = Mock(BaseAttachmentRepository)
+
+        action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
+            Mock(BaseAttachmentFactory),
+            attachment_repository,
+            melding_repository,
+            filesystem,
+            Mock(TokenVerifier),
+            "/tmp",
+        )
+
+        data = b"Hello World!"
+
+        attachment = await action(123, "super_secret_token", "original_filename.ext", data)
+
+        filesystem.makedirs.assert_awaited_once()
+        filesystem.write.assert_awaited_once_with(attachment.file_path, data)
+        attachment_repository.save.assert_awaited_once_with(attachment)
 
 
-@pytest.mark.anyio
-async def test_can_handle_attachment() -> None:
-    melding = Melding("melding text")
+class TestDownloadAttachmentAction:
+    @pytest.mark.anyio
+    async def test_melding_not_found(self) -> None:
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = None
 
-    melding_repository = Mock(BaseMeldingRepository)
-    melding_repository.retrieve.return_value = melding
+        action: DownloadAttachmentAction[Melding, Melding] = DownloadAttachmentAction(
+            melding_repository,
+            Mock(TokenVerifier),
+            Mock(BaseAttachmentRepository),
+            Mock(Filesystem),
+        )
 
-    filesystem = Mock(Filesystem)
+        with pytest.raises(NotFoundException) as exception_info:
+            await action(123, 456, "supersecrettoken")
 
-    attachment_repository = Mock(BaseAttachmentRepository)
+        assert str(exception_info.value) == "Melding not found"
 
-    action: UploadAttachmentAction[Attachment, Melding, Melding] = UploadAttachmentAction(
-        Mock(BaseAttachmentFactory),
-        attachment_repository,
-        melding_repository,
-        filesystem,
-        Mock(TokenVerifier),
-        "/tmp",
-    )
+    @pytest.mark.anyio
+    async def test_attachment_not_found(self) -> None:
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = Melding(text="text")
 
-    data = b"Hello World!"
+        attachment_repository = Mock(BaseAttachmentRepository)
+        attachment_repository.retrieve.return_value = None
 
-    attachment = await action(123, "super_secret_token", "original_filename.ext", data)
+        action: DownloadAttachmentAction[Melding, Melding] = DownloadAttachmentAction(
+            melding_repository,
+            Mock(TokenVerifier),
+            attachment_repository,
+            Mock(Filesystem),
+        )
 
-    filesystem.makedirs.assert_awaited_once()
-    filesystem.write.assert_awaited_once_with(attachment.file_path, data)
-    attachment_repository.save.assert_awaited_once_with(attachment)
+        with pytest.raises(NotFoundException) as exception_info:
+            await action(123, 456, "supersecrettoken")
+
+        assert str(exception_info.value) == "Attachment not found"
+
+    @pytest.mark.anyio
+    async def test_attachment_does_not_belong_to_melding(self) -> None:
+        melding = Melding(text="text")
+        attachment = Attachment("bla", Melding(text="some text"))
+
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = melding
+
+        attachment_repository = Mock(BaseAttachmentRepository)
+        attachment_repository.retrieve.return_value = attachment
+
+        action: DownloadAttachmentAction[Melding, Melding] = DownloadAttachmentAction(
+            melding_repository,
+            Mock(TokenVerifier),
+            attachment_repository,
+            Mock(Filesystem),
+        )
+
+        with pytest.raises(NotFoundException) as exception_info:
+            await action(123, 456, "supersecrettoken")
+
+        assert str(exception_info.value) == "Melding with id 123 does not have attachment with id 456"
+
+    @pytest.mark.anyio
+    async def test_can_handle_attachment_download(self) -> None:
+        melding = Melding(text="text")
+        attachment = Attachment("bla", melding)
+        attachment.file_path = "/path/to/file.ext"
+
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = melding
+
+        attachment_repository = Mock(BaseAttachmentRepository)
+        attachment_repository.retrieve.return_value = attachment
+
+        action: DownloadAttachmentAction[Melding, Melding] = DownloadAttachmentAction(
+            melding_repository,
+            Mock(TokenVerifier),
+            attachment_repository,
+            Mock(Filesystem),
+        )
+
+        await action(123, 456, "supersecrettoken")
+
+    @pytest.mark.anyio
+    async def test_file_not_found(self) -> None:
+        melding = Melding(text="text")
+        attachment = Attachment("bla", melding)
+        attachment.file_path = "/path/to/file.ext"
+
+        melding_repository = Mock(BaseMeldingRepository)
+        melding_repository.retrieve.return_value = melding
+
+        attachment_repository = Mock(BaseAttachmentRepository)
+        attachment_repository.retrieve.return_value = attachment
+
+        file = Mock(File)
+        file.get_iterator.side_effect = filesystem.NotFoundException
+
+        filesystem_mock = Mock(Filesystem)
+        filesystem_mock.get_file.return_value = file
+
+        action: DownloadAttachmentAction[Melding, Melding] = DownloadAttachmentAction(
+            melding_repository,
+            Mock(TokenVerifier),
+            attachment_repository,
+            filesystem_mock,
+        )
+
+        with pytest.raises(NotFoundException) as exception_info:
+            await action(123, 456, "supersecrettoken")
+
+        assert str(exception_info.value) == "File not found"
