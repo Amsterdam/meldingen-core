09283f70f99c0ec1f201e6e9538efa1f0a4d32e3
Youri Westerman <y.westerman@amsterdam.nl>
2024-10-07 13:57:15 +0200

    Add MIME type validator interface and exception

---
+++ meldingen_core/validators.py
@@ -0,0 +1,10 @@
+from abc import ABCMeta, abstractmethod
+
+
+class MIMETypeNotAllowed(Exception): ...
+
+
+class MIMETypeValidator(metaclass=ABCMeta):
+    @abstractmethod
+    def __call__(self, mime_type: str) -> None:
+        """Checks if the provided MIME type is allowed, raises a MIMETypeNotAllowed exception if not."""
