5eb3fe9328db03a04ee2d14e13da29b777376740
Youri Westerman <y.westerman@amsterdam.nl>
2024-10-07 14:02:28 +0200

    Rename to reflect that the validator is a base class

--- meldingen_core/validators.py
+++ meldingen_core/validators.py
@@ -4,7 +4,7 @@ from abc import ABCMeta, abstractmethod
 class MIMETypeNotAllowed(Exception): ...
 
 
-class MIMETypeValidator(metaclass=ABCMeta):
+class BaseMIMETypeValidator(metaclass=ABCMeta):
     @abstractmethod
     def __call__(self, mime_type: str) -> None:
         """Checks if the provided MIME type is allowed, raises a MIMETypeNotAllowed exception if not."""
