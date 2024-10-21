145e3ec742d14743cee18aa3cd78d6f77ef60044
Youri Westerman <y.westerman@amsterdam.nl>
2024-10-21 16:33:06 +0200

    Modify token verifier tests to reflect the new situation

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,31 +1,59 @@
 from datetime import datetime, timedelta
+from unittest.mock import Mock
 
 import pytest
 
+from meldingen_core.exceptions import NotFoundException
 from meldingen_core.models import Melding
+from meldingen_core.repositories import BaseMeldingRepository
 from meldingen_core.token import InvalidTokenException, TokenExpiredException, TokenVerifier
 
 
-def test_token_invalid() -> None:
+@pytest.mark.anyio
+async def test_melding_not_found() -> None:
+    repository = Mock(BaseMeldingRepository)
+    repository.retrieve.return_value = None
+
+    verify_token: TokenVerifier[Melding, Melding] = TokenVerifier(repository)
+    with pytest.raises(NotFoundException):
+        await verify_token(123, "")
+
+
+@pytest.mark.anyio
+async def test_token_invalid() -> None:
     melding = Melding("text", token="54321")
-    verify_token: TokenVerifier[Melding] = TokenVerifier()
+
+    repository = Mock(BaseMeldingRepository)
+    repository.retrieve.return_value = melding
+
+    verify_token: TokenVerifier[Melding, Melding] = TokenVerifier(repository)
 
     with pytest.raises(InvalidTokenException):
-        verify_token(melding, "12345")
+        await verify_token(123, "12345")
 
 
-def test_token_expired() -> None:
+@pytest.mark.anyio
+async def test_token_expired() -> None:
     token = "123456"
     melding = Melding("text", token=token, token_expires=datetime.now() - timedelta(days=1))
-    verify_token: TokenVerifier[Melding] = TokenVerifier()
+    repository = Mock(BaseMeldingRepository)
+    repository.retrieve.return_value = melding
+
+    verify_token: TokenVerifier[Melding, Melding] = TokenVerifier(repository)
 
     with pytest.raises(TokenExpiredException):
-        verify_token(melding, token)
+        await verify_token(123, token)
 
 
-def test_token_valid() -> None:
+@pytest.mark.anyio
+async def test_token_valid() -> None:
     token = "123456"
-    melding = Melding("text", token=token, token_expires=datetime.now() + timedelta(days=1))
-    verify_token: TokenVerifier[Melding] = TokenVerifier()
+    repo_melding = Melding("text", token=token, token_expires=datetime.now() + timedelta(days=1))
 
-    verify_token(melding, token)
+    repository = Mock(BaseMeldingRepository)
+    repository.retrieve.return_value = repo_melding
+
+    verify_token: TokenVerifier[Melding, Melding] = TokenVerifier(repository)
+
+    melding = await verify_token(123, token)
+    assert melding == repo_melding
