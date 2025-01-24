4ae53db22cd47c0c4ded1534da5ace4727b44d24
Youri Westerman <y.westerman@amsterdam.nl>
2025-01-24 14:48:40 +0100

    Feat/malware scanning (#120)
    * Introduce malware scanner base class
    
    * Force malware scanner to be part of ingestor composition
    
    * Rename property to make more sense in context
    
    * Don't include abstract class in test coverage

--- meldingen_core/image.py
+++ meldingen_core/image.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod  # pragma: no cover
 from typing import AsyncIterator, Generic, TypeVar
 
+from meldingen_core.malware import BaseMalwareScanner
 from meldingen_core.models import Attachment
 
 
@@ -17,6 +18,11 @@ class BaseThumbnailGenerator(metaclass=ABCMeta):  # pragma: no cover
 T = TypeVar("T", bound=Attachment)
 
 
-class BaseIngestor(Generic[T], metaclass=ABCMeta):
+class BaseIngestor(Generic[T], metaclass=ABCMeta):  # pragma: no cover
+    _scan_for_malware: BaseMalwareScanner
+
+    def __init__(self, scanner: BaseMalwareScanner):
+        self._scan_for_malware = scanner
+
     @abstractmethod
     async def __call__(self, attachment: T, data: AsyncIterator[bytes]) -> None: ...
