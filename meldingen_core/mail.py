e4be111bf9e2575f74eb921459abc7dca68b152b
Youri Westerman <y.westerman@amsterdam.nl>
2025-05-09 10:39:11 +0200

    Make mailer class generic so that we can override the melding type

--- meldingen_core/mail.py
+++ meldingen_core/mail.py
@@ -1,11 +1,11 @@
 from abc import ABCMeta, abstractmethod
-from typing import TypeVar
+from typing import Generic, TypeVar
 
 from meldingen_core.models import Melding
 
 T = TypeVar("T", bound=Melding)
 
 
-class BaseMeldingConfirmationMailer(metaclass=ABCMeta):
+class BaseMeldingConfirmationMailer(Generic[T], metaclass=ABCMeta):
     @abstractmethod
     async def __call__(self, melding: T) -> None: ...
