4b609e4e79aab693d347738955bcad582547457c
ramonavic <r.hofmeister@amsterdam.nl>
2025-05-21 11:35:18 +0200

    save address data with tests

---
+++ tests/test_location.py
@@ -0,0 +1,42 @@
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+
+from meldingen_core.location import AddressSchema, BaseAddressProviderTask, BaseAddressResolver
+from meldingen_core.models import Melding
+from meldingen_core.repositories import BaseMeldingRepository
+
+
+@pytest.mark.anyio
+async def test_address_provider() -> None:
+    melding = Melding("My Melding")
+    lat, lon = 52.3791283, 4.900272
+
+    resolve_address = AsyncMock(BaseAddressResolver)
+    resolve_address.return_value = AddressSchema(
+        street="Stationsplein", house_number=39, house_number_addition="K", postal_code="1012AB", city="Amsterdam"
+    )
+
+    repository = Mock(BaseMeldingRepository)
+
+    provide_address: BaseAddressProviderTask[Melding] = BaseAddressProviderTask(resolve_address, repository)
+
+    await provide_address(melding, lat, lon)
+    resolve_address.assert_called_once_with(lat, lon)
+    repository.save.assert_awaited_once_with(melding)
+
+
+@pytest.mark.anyio
+async def test_address_provider_without_address() -> None:
+    melding = Melding("Afval op de stoep")
+    lat, lon = 52.3791283, 4.900272
+
+    resolve_address = AsyncMock(BaseAddressResolver)
+    resolve_address.return_value = None
+
+    repository = AsyncMock(BaseMeldingRepository)
+
+    provide_address: BaseAddressProviderTask[Melding] = BaseAddressProviderTask(resolve_address, repository)
+
+    await provide_address(melding, lat, lon)
+    resolve_address.assert_called_once_with(lat, lon)
