78a27461cc6d6fc1df93ef717905e1d05835ad61
ramonavic <r.hofmeister@amsterdam.nl>
2025-05-26 16:49:51 +0200

    resolver can also return none

--- meldingen_core/address.py
+++ meldingen_core/address.py
@@ -22,7 +22,7 @@ class BaseAddressResolver(metaclass=ABCMeta):
     """Adapter responsible for getting the address data from another source"""
 
     @abstractmethod
-    async def __call__(self, lon: float, lat: float) -> Address: ...
+    async def __call__(self, lon: float, lat: float) -> Address | None: ...
 
 
 class BaseAddressEnricher(Generic[T], metaclass=ABCMeta):
