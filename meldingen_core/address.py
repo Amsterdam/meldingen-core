14b60480558945af10a2eb6b466fd7021b39bf8b
ramonavic <ramon.hofmeister@gmail.com>
2025-05-27 14:18:58 +0200

    save address data with tests (#153)
    * save address data with tests
    
    * remove implementation from core
    
    * give address its own
    
    * remove old tests since we dont have implementation
    
    * dont use TypeVar for address
    
    * resolver can also return none
    
    * keep order of params the same for superclass
    
    * no test coverage on base classes of address
    
    * formatting and fix no cover
    
    * make Address Generic
    
    * make class generic
    
    * dont bind generic class to Address dataclass
    
    * bind to Address
    
    * add more lines to no cover
    
    * add address generic to BaseAddressEnricher

---
+++ meldingen_core/address.py
@@ -0,0 +1,40 @@
+from abc import ABCMeta, abstractmethod  # pragma: no cover
+from dataclasses import dataclass  # pragma: no cover
+from typing import Generic, TypeVar  # pragma: no cover
+
+from meldingen_core.models import Melding  # pragma: no cover
+from meldingen_core.repositories import BaseMeldingRepository  # pragma: no cover
+
+
+@dataclass  # pragma: no cover
+class Address:
+    city: str
+    postal_code: str
+    street: str
+    house_number: int
+    house_number_addition: str | None = None
+
+
+T = TypeVar("T", bound=Melding)  # pragma: no cover
+A = TypeVar("A", bound=Address)  # pragma: no cover
+
+
+class BaseAddressResolver(Generic[A], metaclass=ABCMeta):  # pragma: no cover
+    """Adapter responsible for getting the address data from another source"""
+
+    @abstractmethod
+    async def __call__(self, lat: float, lon: float) -> A | None: ...
+
+
+class BaseAddressEnricher(Generic[T, A], metaclass=ABCMeta):  # pragma: no cover
+    """Takes a coordinate and adds its address data to the melding"""
+
+    _resolve_address: BaseAddressResolver[A]
+    _repository: BaseMeldingRepository[T]
+
+    def __init__(self, resolve_address: BaseAddressResolver[A], repository: BaseMeldingRepository[T]) -> None:
+        self._resolve_address = resolve_address
+        self._repository = repository
+
+    @abstractmethod
+    async def __call__(self, melding: T, lat: float, lon: float) -> None: ...
