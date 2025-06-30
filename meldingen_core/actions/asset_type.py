33ff95c2e7995b745614b50270ee2519b9e85550
Youri Westerman <y.westerman@amsterdam.nl>
2025-06-30 16:03:10 +0200

    Add `AssetTypeRetrieveAction`

--- meldingen_core/actions/asset_type.py
+++ meldingen_core/actions/asset_type.py
@@ -1,12 +1,15 @@
 from typing import TypeVar
 
-from meldingen_core.actions.base import BaseCreateAction, BaseDeleteAction, BaseUpdateAction
+from meldingen_core.actions.base import BaseCreateAction, BaseDeleteAction, BaseRetrieveAction, BaseUpdateAction
 from meldingen_core.models import AssetType
 
 AT = TypeVar("AT", bound=AssetType)
 
 
 class AssetTypeCreateAction(BaseCreateAction[AT]): ...
+
+
+class AssetTypeRetrieveAction(BaseRetrieveAction[AT]): ...
 
 
 class AssetTypeUpdateAction(BaseUpdateAction[AT]): ...
