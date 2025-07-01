1e6d5fbb805a06c9beb9bdc58dee5e0e5664cf09
Youri Westerman <y.westerman@amsterdam.nl>
2025-07-01 15:17:02 +0200

    Add abstract WFS provider base class

---
+++ meldingen_core/wfs.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta, abstractmethod
+from typing import AsyncIterator
+
+from meldingen_core.models import AssetType
+
+
+class BaseWfsProvider(metaclass=ABCMeta):
+    @abstractmethod
+    async def __call__(
+        self,
+        type_names: str,
+        count: int = 1000,
+        srs_name: str = "urn:ogc:def:crs:EPSG::4326",
+        output_format: str = "application/json",
+        filter: str | None = None,
+    ) -> tuple[AsyncIterator[bytes], str]:
+        ...
