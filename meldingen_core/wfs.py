42465d982fd31b1ecb0e354910c71a3a789c8363
Youri Westerman <y.westerman@amsterdam.nl>
2025-07-01 15:40:38 +0200

    Implement `WfsProviderFactory`

--- meldingen_core/wfs.py
+++ meldingen_core/wfs.py
@@ -1,5 +1,8 @@
 from abc import ABCMeta, abstractmethod
+from importlib import import_module
 from typing import AsyncIterator
+
+from meldingen_core.models import AssetType
 
 
 class BaseWfsProvider(metaclass=ABCMeta):
@@ -11,5 +14,19 @@ class BaseWfsProvider(metaclass=ABCMeta):
         srs_name: str = "urn:ogc:def:crs:EPSG::4326",
         output_format: str = "application/json",
         filter: str | None = None,
-    ) -> tuple[AsyncIterator[bytes], str]:
-        ...
+    ) -> tuple[AsyncIterator[bytes], str]: ...
+
+
+class InvalidWfsProviderException(Exception): ...
+
+
+class WfsProviderFactory:
+    def __call__(self, asset_type: AssetType) -> BaseWfsProvider:
+        module_name, class_name = asset_type.class_name.rsplit(".", 1)
+        module = import_module(module_name)
+
+        provider = getattr(module, class_name)(**asset_type.arguments)
+        if not isinstance(provider, BaseWfsProvider):
+            raise InvalidWfsProviderException("Invalid Wfs Provider")
+
+        return provider
