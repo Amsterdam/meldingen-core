a965bbbc05443928b173a414dfd5cace7c2b5e4a
Joey <joey.hendriks@amsterdam.nl>
2025-07-04 14:19:37 +0200

    run black and poetry

--- tests/test_actions/test_wfs.py
+++ tests/test_actions/test_wfs.py
@@ -6,7 +6,7 @@ from meldingen_core.actions.wfs import WfsRetrieveAction
 from meldingen_core.exceptions import NotFoundException
 from meldingen_core.models import AssetType
 from meldingen_core.repositories import BaseAssetTypeRepository
-from meldingen_core.wfs import WfsProviderFactory, BaseWfsProvider
+from meldingen_core.wfs import BaseWfsProvider, WfsProviderFactory
 
 
 class TestWfsAction:
@@ -30,7 +30,7 @@ class TestWfsAction:
 
         repository.find_by_name.assert_awaited_once_with("test")
         factory.assert_called_once_with(asset_type_mock)
-        provider.assert_awaited_once_with('test', 1000, 'urn:ogc:def:crs:EPSG::4326', 'application/json', None)
+        provider.assert_awaited_once_with("test", 1000, "urn:ogc:def:crs:EPSG::4326", "application/json", None)
 
     @pytest.mark.anyio
     async def test_asset_type_not_found(self) -> None:
