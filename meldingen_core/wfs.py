644582f52a637e8ed6b62905ab0d399d6a60fc1f
Joey <joey.hendriks@amsterdam.nl>
2025-07-08 11:29:25 +0200

    Fix literal application/json for output format wfs

--- meldingen_core/wfs.py
+++ meldingen_core/wfs.py
@@ -12,7 +12,7 @@ class BaseWfsProvider(metaclass=ABCMeta):
         type_names: str,
         count: int = 1000,
         srs_name: str = "urn:ogc:def:crs:EPSG::4326",
-        output_format: str = "application/json",
+        output_format: Literal["application/json"] = "application/json",
         service: Literal["WFS"] = "WFS",
         version: str = "2.0.0",
         request: Literal["GetFeature"] = "GetFeature",
