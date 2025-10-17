9b3fca6671e82612f4d94712f88489575ac0f44f
ramonavic <r.hofmeister@amsterdam.nl>
2025-10-17 13:59:38 +0200

    corret naming

--- tests/test_actions/test_asset.py
+++ tests/test_actions/test_asset.py
@@ -13,28 +13,28 @@ class TestListAssetsAction:
     @pytest.mark.anyio
     async def test_can_list_assets(self) -> None:
         melding_id = 123
-        repo_attachments: list[Asset] = []
+        repo_assets: list[Asset] = []
         repository = Mock(BaseAssetRepository)
-        repository.find_by_melding.return_value = repo_attachments
+        repository.find_by_melding.return_value = repo_assets
 
         action: ListAssetsAction[Asset] = ListAssetsAction(repository)
-        attachments = await action(melding_id)
+        assets = await action(melding_id)
 
-        assert repo_attachments == attachments
+        assert repo_assets == assets
         repository.find_by_melding.assert_awaited_once_with(melding_id)
 
     @pytest.mark.anyio
     async def test_melder_can_list_attachments(self) -> None:
         token = "supersecrettoken"
         melding_id = 123
-        repo_attachments: list[Asset] = []
+        repo_assets: list[Asset] = []
         token_verifier = AsyncMock(TokenVerifier)
         repository = Mock(BaseAssetRepository)
-        repository.find_by_melding.return_value = repo_attachments
+        repository.find_by_melding.return_value = repo_assets
 
         action: MelderListAssetsAction[Asset, Melding] = MelderListAssetsAction(token_verifier, repository)
-        attachments = await action(melding_id, token)
+        assets = await action(melding_id, token)
 
-        assert repo_attachments == attachments
+        assert repo_assets == assets
         token_verifier.assert_awaited_once()
         repository.find_by_melding.assert_awaited_once_with(melding_id)
