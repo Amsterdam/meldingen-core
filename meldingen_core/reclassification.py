c2d134cd57885aedffba8d42c9a44827f5ac1b4d
ramonavic <r.hofmeister@amsterdam.nl>
2025-12-19 11:07:51 +0100

    reset to older version

--- meldingen_core/reclassification.py
+++ meldingen_core/reclassification.py
@@ -9,5 +9,5 @@ C = TypeVar("C", bound=Classification)
 
 class BaseReclassification(Generic[M, C], metaclass=ABCMeta):
     @abstractmethod
-    async def __call__(self, melding: M, old_classification: C | None, new_classification: C | None) -> M:
+    async def __call__(self, melding: M, old_classification: C | None, new_classification: C | None) -> None:
         """Handle reclassification side effects here, like changing the assets or removing/changing the location."""
