0ea2c05afe10c55f821040d098b2663ad939ccb0
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-09-09 15:51:35 +0200

    Removed tokenverification and related Melder Class

--- tests/test_actions/test_attachment.py
+++ tests/test_actions/test_attachment.py
@@ -10,10 +10,7 @@ from meldingen_core.actions.attachment import (
     DeleteAttachmentAction,
     DownloadAttachmentAction,
     ListAttachmentsAction,
-    MelderDeleteAttachmentAction,
-    MelderDownloadAttachmentAction,
     MelderListAttachmentsAction,
-    MelderUploadAttachmentAction,
     UploadAttachmentAction,
 )
 from meldingen_core.exceptions import NotFoundException
@@ -21,7 +18,6 @@ from meldingen_core.factories import BaseAttachmentFactory
 from meldingen_core.image import BaseIngestor
 from meldingen_core.models import Attachment, Melding, User
 from meldingen_core.repositories import BaseAttachmentRepository, BaseMeldingRepository
-from meldingen_core.token import TokenVerifier
 from meldingen_core.validators import (
     AttachmentLimitReachedException,
     BaseAttachmentLimitValidator,
@@ -33,252 +29,6 @@ from meldingen_core.validators import (
 async def _iterator() -> AsyncIterator[bytes]:
     for chunk in [b"Hello ", b"world", b"!"]:
         yield chunk
-
-
-class TestMelderUploadAttachmentAction:
-    @pytest.mark.anyio
-    async def test_can_handle_attachment(self) -> None:
-        melding = Melding("melding text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment_repository = Mock(BaseAttachmentRepository[Attachment])
-        attachment_repository.save = AsyncMock()
-
-        action: MelderUploadAttachmentAction[Attachment, Melding, User] = MelderUploadAttachmentAction(
-            token_verifier,
-            Mock(BaseAttachmentFactory),
-            attachment_repository,
-            Mock(BaseMediaTypeValidator),
-            Mock(BaseMediaTypeIntegrityValidator),
-            AsyncMock(BaseAttachmentLimitValidator[Melding]),
-            AsyncMock(BaseIngestor),
-        )
-
-        iterator = _iterator()
-
-        attachment = await action(123, "super_secret_token", "original_filename.ext", "image/png", b"test", iterator)
-
-        attachment_repository.save.assert_awaited_once_with(attachment)
-
-    @pytest.mark.anyio
-    async def test_raises_when_attachment_limit_is_reached(self) -> None:
-        melding = Melding("melding text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment_repository = Mock(BaseAttachmentRepository[Attachment])
-        attachment_repository.save = AsyncMock()
-
-        attachment_limit_validator = AsyncMock(BaseAttachmentLimitValidator[Melding])
-        attachment_limit_validator.side_effect = AttachmentLimitReachedException()
-        ingestor = AsyncMock(BaseIngestor)
-
-        action: MelderUploadAttachmentAction[Attachment, Melding, User] = MelderUploadAttachmentAction(
-            token_verifier,
-            Mock(BaseAttachmentFactory),
-            attachment_repository,
-            Mock(BaseMediaTypeValidator),
-            Mock(BaseMediaTypeIntegrityValidator),
-            attachment_limit_validator,
-            ingestor,
-        )
-
-        iterator = _iterator()
-
-        with pytest.raises(AttachmentLimitReachedException):
-            await action(123, "super_secret_token", "original_filename.ext", "image/png", b"test", iterator)
-
-        attachment_limit_validator.assert_awaited_once_with(melding)
-        ingestor.assert_not_awaited()
-        attachment_repository.save.assert_not_awaited()
-
-
-class TestMelderDownloadAttachmentAction:
-    @pytest.mark.anyio
-    async def test_attachment_not_found(self) -> None:
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = None
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            AsyncMock(TokenVerifier),
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken", AttachmentTypes.ORIGINAL)
-
-        assert str(exception_info.value) == "Attachment not found"
-
-    @pytest.mark.anyio
-    async def test_attachment_does_not_belong_to_melding(self) -> None:
-        attachment = Attachment(
-            id=1, original_filename="bla", original_media_type="image/png", melding=Melding(text="some text")
-        )
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            AsyncMock(TokenVerifier),
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken", AttachmentTypes.ORIGINAL)
-
-        assert str(exception_info.value) == "Melding with id 123 does not have attachment with id 456"
-
-    @pytest.mark.anyio
-    @pytest.mark.parametrize("_type", AttachmentTypes)
-    async def test_can_handle_attachment_download(self, _type: AttachmentTypes) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-        attachment.original_media_type = "image/png"
-        attachment.optimized_path = "/path/to/file-optimized.ext"
-        attachment.optimized_media_type = "image/webp"
-        attachment.thumbnail_path = "/path/to/file-thumbnail.ext"
-        attachment.thumbnail_media_type = "image/webp"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        await action(123, 456, "supersecrettoken", _type)
-
-    @pytest.mark.anyio
-    async def test_optimized_path_none(self) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken", AttachmentTypes.OPTIMIZED)
-
-        assert str(exception_info.value) == "Optimized file not found"
-
-    @pytest.mark.anyio
-    async def test_optimized_media_type_none(self) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-        attachment.optimized_path = "/path/to/file-optimized.ext"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken", AttachmentTypes.OPTIMIZED)
-
-        assert str(exception_info.value) == "Optimized media type not found"
-
-    @pytest.mark.anyio
-    async def test_thumbnail_path_none(self) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken", AttachmentTypes.THUMBNAIL)
-
-        assert str(exception_info.value) == "Thumbnail file not found"
-
-    @pytest.mark.anyio
-    async def test_thumbnail_media_type_none(self) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-        attachment.thumbnail_path = "/path/to/file-thumbnail.ext"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken", AttachmentTypes.THUMBNAIL)
-
-        assert str(exception_info.value) == "Thumbnail media type not found"
-
-    @pytest.mark.anyio
-    async def test_file_not_found(self) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        file = Mock(File)
-        file.get_iterator.side_effect = filesystem.NotFoundException
-
-        filesystem_mock = Mock(Filesystem)
-        filesystem_mock.get_file.return_value = file
-
-        action: MelderDownloadAttachmentAction[Attachment, Melding] = MelderDownloadAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            filesystem_mock,
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken", AttachmentTypes.ORIGINAL)
-
-        assert str(exception_info.value) == "File not found"
 
 
 class TestDownloadAttachmentAction:
@@ -323,8 +73,6 @@ class TestDownloadAttachmentAction:
     @pytest.mark.anyio
     async def test_optimized_path_none(self) -> None:
         melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
 
         attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
         attachment.file_path = "/path/to/file.ext"
@@ -443,110 +191,20 @@ class TestListAttachmentsAction:
 
     @pytest.mark.anyio
     async def test_melder_can_list_attachments(self) -> None:
-        token = "supersecrettoken"
         melding_id = 123
         repo_attachments: list[Attachment] = []
-        token_verifier = AsyncMock(TokenVerifier)
-        repository = Mock(BaseAttachmentRepository)
-        repository.find_by_melding.return_value = repo_attachments
+        melding_repository = AsyncMock(BaseMeldingRepository)
+        attachment_repository = Mock(BaseAttachmentRepository)
+        attachment_repository.find_by_melding.return_value = repo_attachments
 
         action: MelderListAttachmentsAction[Attachment, Melding] = MelderListAttachmentsAction(
-            token_verifier, repository
+            melding_repository, attachment_repository
         )
-        attachments = await action(melding_id, token)
+        attachments = await action(melding_id)
 
         assert repo_attachments == attachments
-        token_verifier.assert_awaited_once()
-        repository.find_by_melding.assert_awaited_once_with(melding_id)
-
-
-class TestMelderDeleteAttachmentAction:
-    @pytest.mark.anyio
-    async def test_attachment_not_found(self) -> None:
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = None
-
-        action: MelderDeleteAttachmentAction[Attachment, Melding] = MelderDeleteAttachmentAction(
-            AsyncMock(TokenVerifier),
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken")
-
-        assert str(exception_info.value) == "Attachment not found"
-
-    @pytest.mark.anyio
-    async def test_attachment_does_not_belong_to_melding(self) -> None:
-        attachment = Attachment(
-            id=1, original_filename="bla", original_media_type="image/png", melding=Melding(text="some text")
-        )
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        action: MelderDeleteAttachmentAction[Attachment, Melding] = MelderDeleteAttachmentAction(
-            AsyncMock(TokenVerifier),
-            attachment_repository,
-            Mock(Filesystem),
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken")
-
-        assert str(exception_info.value) == "Melding with id 123 does not have attachment with id 456"
-
-    @pytest.mark.anyio
-    async def test_file_not_found(self) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        filesystem_mock = Mock(Filesystem)
-        filesystem_mock.delete.side_effect = filesystem.NotFoundException
-
-        action: MelderDeleteAttachmentAction[Attachment, Melding] = MelderDeleteAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            filesystem_mock,
-        )
-
-        with pytest.raises(NotFoundException) as exception_info:
-            await action(123, 456, "supersecrettoken")
-
-        assert str(exception_info.value) == "File not found"
-
-    @pytest.mark.anyio
-    async def test_delete_attachment(self) -> None:
-        melding = Melding(text="text")
-        token_verifier = AsyncMock(TokenVerifier)
-        token_verifier.return_value = melding
-
-        attachment = Attachment(id=1, original_filename="bla", original_media_type="image/png", melding=melding)
-        attachment.file_path = "/path/to/file.ext"
-
-        attachment_repository = Mock(BaseAttachmentRepository)
-        attachment_repository.retrieve.return_value = attachment
-
-        filesystem_mock = Mock(Filesystem)
-
-        action: MelderDeleteAttachmentAction[Attachment, Melding] = MelderDeleteAttachmentAction(
-            token_verifier,
-            attachment_repository,
-            filesystem_mock,
-        )
-
-        await action(123, 456, "supersecrettoken")
-
-        filesystem_mock.delete.assert_awaited_once_with(attachment.file_path)
-        attachment_repository.delete.assert_awaited_once_with(attachment.id)
+        melding_repository.retrieve.assert_awaited_once()
+        attachment_repository.find_by_melding.assert_awaited_once_with(melding_id)
 
 
 class TestDeleteAttachmentAction:
@@ -622,20 +280,19 @@ class TestUploadAttachmentAction:
 
         user = User(id=1, username="behandelaar", email="user@example.com")
 
-        attachment_repository = Mock(BaseAttachmentRepository[Attachment])
-        attachment_repository.save = AsyncMock()
+        attachment_repository = AsyncMock(BaseAttachmentRepository)
 
         melding_repository = AsyncMock(BaseMeldingRepository)
         melding_repository.retrieve.return_value = melding
 
         action: UploadAttachmentAction[Attachment, Melding, User] = UploadAttachmentAction(
+            melding_repository,
             Mock(BaseAttachmentFactory),
             attachment_repository,
             Mock(BaseMediaTypeValidator),
             Mock(BaseMediaTypeIntegrityValidator),
             AsyncMock(BaseAttachmentLimitValidator),
             AsyncMock(BaseIngestor),
-            melding_repository,
         )
 
         iterator = _iterator()
@@ -663,13 +320,13 @@ class TestUploadAttachmentAction:
         ingestor = AsyncMock(BaseIngestor)
 
         action: UploadAttachmentAction[Attachment, Melding, User] = UploadAttachmentAction(
+            melding_repository,
             Mock(BaseAttachmentFactory),
             attachment_repository,
             Mock(BaseMediaTypeValidator),
             Mock(BaseMediaTypeIntegrityValidator),
             attachment_limit_validator,
             ingestor,
-            melding_repository,
         )
 
         iterator = _iterator()
@@ -695,13 +352,13 @@ class TestUploadAttachmentAction:
         melding_repository.retrieve.return_value = None
 
         action: UploadAttachmentAction[Attachment, Melding, User] = UploadAttachmentAction(
+            melding_repository,
             Mock(BaseAttachmentFactory),
             attachment_repository,
             Mock(BaseMediaTypeValidator),
             Mock(BaseMediaTypeIntegrityValidator),
             AsyncMock(BaseAttachmentLimitValidator[Melding]),
             AsyncMock(BaseIngestor),
-            melding_repository,
         )
 
         iterator = _iterator()
