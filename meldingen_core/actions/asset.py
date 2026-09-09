8e8cb6cecb1c362e84a92e24f2c7886960c7ef95
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-09-09 17:00:26 +0200

    Rename

--- meldingen_core/actions/asset.py
+++ meldingen_core/actions/asset.py
@@ -7,14 +7,16 @@ from meldingen_core.repository_helpers import retrieve_or_raise_not_found
 
 
 class ListAssetsAction[A: Asset, M: Melding]:
-    _repository: BaseMeldingRepository[M]
+    _melding_repository: BaseMeldingRepository[M]
     _relationship_manager: RelationshipManager[M, A]
 
-    def __init__(self, repository: BaseMeldingRepository[M], relationship_manager: RelationshipManager[M, A]) -> None:
-        self._repository = repository
+    def __init__(
+        self, melding_repository: BaseMeldingRepository[M], relationship_manager: RelationshipManager[M, A]
+    ) -> None:
+        self._melding_repository = melding_repository
         self._relationship_manager = relationship_manager
 
     async def __call__(self, melding_id: int) -> Sequence[A]:
-        melding = await retrieve_or_raise_not_found(self._repository, melding_id)
+        melding = await retrieve_or_raise_not_found(self._melding_repository, melding_id)
 
         return await self._relationship_manager.get_related(melding)
