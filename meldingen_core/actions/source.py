1d4f503e4d3e8322c82294c9c7d5ad88b1d82472
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-09-09 14:39:33 +0200

    Finalize type syntax conversion

--- meldingen_core/actions/source.py
+++ meldingen_core/actions/source.py
@@ -1,10 +1,6 @@
-from typing import TypeVar
-
 from meldingen_core.actions.base import BaseListAction
 from meldingen_core.models import Source
 
-T = TypeVar("T", bound=Source)
 
-
-class SourceListAction(BaseListAction[T]):
+class SourceListAction[T: Source](BaseListAction[T]):
     """Action that retrieves a list of sources."""
